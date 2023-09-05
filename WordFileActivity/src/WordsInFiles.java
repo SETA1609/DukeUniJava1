4c85d67cdd8f15313944eb39edaa422597378090
seta1609 <sebastian.stp16@gmail.com>
2023-09-05 11:21:53 +0200

    ft : maxNumber method was implemented

--- WordFileActivity/src/WordsInFiles.java
+++ WordFileActivity/src/WordsInFiles.java
@@ -5,21 +5,23 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class WordsInFiles {
     private HashMap<String, ArrayList<String>> wordToFileNamesMap;
 
     public WordsInFiles() {
-        wordToFileNamesMap=new HashMap<>();
+        wordToFileNamesMap = new HashMap<>();
     }
-    private void addWordsFromFile(File f){
-        String fileName=f.getName();
-        FileResource fr = new FileResource(f);
-        String[] fileWords=fr.asString().toLowerCase().split(" ");
-        ArrayList<String>currentFile=new ArrayList<>(List.of(fileWords));
-        for (String word:currentFile) {
 
-            ArrayList<String>filenames=wordToFileNamesMap.get(word);
+    private void addWordsFromFile(File f) {
+        String fileName = f.getName();
+        FileResource fr = new FileResource(f);
+        String[] fileWords = fr.asString().toLowerCase().split(" ");
+        ArrayList<String> currentFile = new ArrayList<>(List.of(fileWords));
+        for (String word : currentFile) {
+
+            ArrayList<String> filenames = wordToFileNamesMap.get(word);
 
             if (filenames == null) {
                 filenames = new ArrayList<>();
@@ -32,12 +34,27 @@ public class WordsInFiles {
         }
 
     }
-    public void buildWordFileMap(){
+
+    public void buildWordFileMap() {
         wordToFileNamesMap.clear();
         DirectoryResource dr = new DirectoryResource();
-        for (File f:dr.selectedFiles()) {
+        for (File f : dr.selectedFiles()) {
             addWordsFromFile(f);
         }
     }
+
+    public int maxNumber() {
+        int biggestNumber=0;
+
+        for (Map.Entry<String,ArrayList<String>> entry:wordToFileNamesMap.entrySet()) {
+            int currentSize=entry.getValue().size();
+            if (biggestNumber<currentSize){
+                biggestNumber=currentSize;
+            }
+        }
+
+        return biggestNumber;
+    }
+
 
 }
