3bfff4662a8f463a2f4722cc02e3404a005de232
seta1609 <sebastian.stp16@gmail.com>
2023-09-05 11:14:24 +0200

    ft : addWordsFromFile was modified to use all the words from a file and the buildWordFileMap method was implemented

--- WordFileActivity/src/WordsInFiles.java
+++ WordFileActivity/src/WordsInFiles.java
@@ -1,6 +1,10 @@
+import edu.duke.DirectoryResource;
+import edu.duke.FileResource;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class WordsInFiles {
     private HashMap<String, ArrayList<String>> wordToFileNamesMap;
@@ -9,17 +13,31 @@ public class WordsInFiles {
         wordToFileNamesMap=new HashMap<>();
     }
     private void addWordsFromFile(File f){
-        String word=f.getName();
-        String path=f.getPath();
-        ArrayList<String>filenames=wordToFileNamesMap.get(word);
+        String fileName=f.getName();
+        FileResource fr = new FileResource(f);
+        String[] fileWords=fr.asString().toLowerCase().split(" ");
+        ArrayList<String>currentFile=new ArrayList<>(List.of(fileWords));
+        for (String word:currentFile) {
 
-        if (filenames == null) {
-            filenames = new ArrayList<>();
-            wordToFileNamesMap.put(word, filenames);
+            ArrayList<String>filenames=wordToFileNamesMap.get(word);
+
+            if (filenames == null) {
+                filenames = new ArrayList<>();
+                wordToFileNamesMap.put(word, filenames);
+            }
+
+            if (!filenames.contains(fileName)) {
+                filenames.add(fileName);
+            }
         }
 
-        if (!filenames.contains(path)) {
-            filenames.add(path);
+    }
+    public void buildWordFileMap(){
+        wordToFileNamesMap.clear();
+        DirectoryResource dr = new DirectoryResource();
+        for (File f:dr.selectedFiles()) {
+            addWordsFromFile(f);
         }
     }
+
 }
